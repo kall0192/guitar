f293197f0393a3db01f0304e6682a86f8a9f1c4e
Kalle Mersh <kalle.mersh@live.dk>
2022-09-02 11:45:46 +0200

    Guitar

---
+++ controllers/controller.js
@@ -0,0 +1,76 @@
+export default class Controller {
+    initialize(model, view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    buildTemplate(guitar) {
+        return `<tr class="customerrow">
+        <td>${guitar.getSerialNumber()}</td>
+        <td>${guitar.getBuilder()}</td>
+        <td>${guitar.getModel()}</td>
+        <td>${guitar.getType()}</td>
+        <td>${guitar.getBackWood()}</td>
+        <td>${guitar.getTopWood()}</td>
+        <td>${guitar.getPrice()}</td>
+        <td><button type="button" id="${guitar.getSerialNumber()}">Delete</button></td>
+        </tr>
+        `
+    }
+
+    snSearch(SerialNumber) {
+        const guitar = this.model.guitarList.getGuitar(SerialNumber);
+        let template = "";
+
+        if (guitar !== null) {
+            template = this.buildTemplate(guitar);
+        } else {
+            template = `
+            <tr class="customerrow">
+            <td colspan="8">Nothing to show</td>
+            </tr>`;
+        }
+        this.view.message(template);
+    }
+
+
+search(searchGuitar) {
+    const guitar = this.model.guitarList.search(searchGuitar);
+    let template = "";
+
+    if (guitar !== null) {
+        template = this.buildTemplate(guitar);
+    } else {
+        template = `
+        <tr class="customerrow">
+        <td colspan="8">Nothing to show</td>
+        </tr>`;
+    }
+    this.view.message(template);
+}
+
+    showAllGuitars() {
+        let template = "";
+        for (const guitar of this.model.guitarList.allGuitars()) {
+            template += this.buildTemplate(guitar);
+        }
+        this.view.message(template);
+    }
+
+    newGuitar(guitar) {
+        const doesGuitarAlreadyExist = this.model.guitarList.getGuitar(guitar.serialNumber);
+
+        if (doesGuitarAlreadyExist === null) {
+            this.model.guitarList.addGuitar(guitar.serialNumber, guitar.price, guitar.builder, guitar.model, guitar.type, guitar.backwood, guitar.topwood);
+            this.view.snackbar("The guitar was saved");
+            this.showAllGuitars();
+        } else {
+            this.view.snackbar("The guitar already exist");
+        }
+    }
+
+    deleteGuitar(sn) {
+        this.model.guitarList.deleteGuitar(sn);
+        this.view.snackbar("The guitar was deleted!");
+    }
+}
